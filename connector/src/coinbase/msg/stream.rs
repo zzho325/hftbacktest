64dc773340d7ddd02420cbbdfd41e268b4814e78
Ashley Zhou <ashley.zhou@samsara.com>
2025-06-07 16:31:59 -0700

    feat(rust): refactor Coinbase market_data_stream
    - Refactor sequence number tracking.
    - Add heartbeat channel subscription.
    - Add unit text for level2 message parsing.

--- connector/src/coinbase/msg/stream.rs
+++ connector/src/coinbase/msg/stream.rs
@@ -1,6 +1,5 @@
 use chrono::{DateTime, Utc};
-use serde::Deserialize;
-use serde_json::Value;
+use serde::{Deserialize, Deserializer, de::Error as DeError};
 
 use crate::utils::from_str_to_f64;
 
@@ -10,7 +9,9 @@ pub enum Stream {
     #[serde(rename = "subscriptions")]
     Subscriptions { events: Vec<SubscriptionEvent> },
     #[serde(rename = "l2_data")]
-    Level2(Level2),
+    Level2(ChannelMessage<Level2>),
+    #[serde(rename = "heartbeats")]
+    Heartbeat(ChannelMessage<Heartbeat>),
 }
 
 #[derive(Debug, Deserialize)]
@@ -19,19 +20,19 @@ pub struct SubscriptionEvent {
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Level2 {
+pub struct ChannelMessage<T> {
     pub client_id: String,
     pub timestamp: DateTime<Utc>,
     pub sequence_num: u64,
-    pub events: Vec<Event>,
+    pub events: Vec<T>,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Event {
+pub struct Level2 {
     #[serde(rename = "type")]
     pub type_: String,
     pub product_id: String,
-    pub updates: Vec<Update>,
+    pub updates: Vec<L2Update>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -43,11 +44,33 @@ pub enum Side {
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Update {
+pub struct L2Update {
     pub side: Side,
     pub event_time: DateTime<Utc>,
     #[serde(deserialize_with = "from_str_to_f64")]
     pub price_level: f64,
     #[serde(deserialize_with = "from_str_to_f64")]
     pub new_quantity: f64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Heartbeat {
+    #[serde(deserialize_with = "deserialize_heartbeat_timestamp")]
+    pub current_time: DateTime<Utc>,
+    pub heartbeat_counter: u64,
+}
+
+/// Deserialize heartbeat current_time of format
+/// "2025-06-06 07:01:53.305743326 +0000 UTC m=+27862.572888510".
+fn deserialize_heartbeat_timestamp<'de, D>(d: D) -> Result<DateTime<Utc>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(d)?;
+    let trimmed = s.split(" m=").next().ok_or_else(|| {
+        <D::Error as serde::de::Error>::custom("invalid Go timestamp: missing ` m=` delimiter")
+    })?;
+    DateTime::parse_from_str(trimmed, "%Y-%m-%d %H:%M:%S%.f %z UTC")
+        .map_err(serde::de::Error::custom)
+        .map(|dt| dt.with_timezone(&Utc))
 }
